d06e6b0f0855e318cb382457e2578d9a2960f076
wayne <www_wayne_001@163.com>
2017-08-04 18:04:13 +0800

    #添加雾

---
+++ Assets/Script/MoveCloud.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCloud : MonoBehaviour {
+
+    public GameObject ball;
+
+    private float offsetZ;
+
+    // Use this for initialization
+    void Start () {
+        offsetZ = transform.position.z - ball.transform.position.z;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position= new Vector3(transform.position.x, transform.position.y , ball.transform.position.z + offsetZ);
+
+    }
+}
